32dac0822bec978a7d357025fcb09ba0420c5a06
Magdy156 <magdyhamdy156@gmail.com>
2023-11-30 02:19:43 +0200

    Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import {  LightModeOutlined, DarkModeOutlined, Menu as Toggle, Search, SettingsOutlined, ArrowDropDropDownOutlined, Mode } from "@mui/icons-material";
+import FlexBetween from './FlexBetween';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'state';
+import profileImage from "assets/bla.jpeg"
+import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
+
+
+const Navbar = () => {
+    const dispatch = useDispatch();
+    const theme = useTheme();
+
+  return (
+    <AppBar
+    sx = {{
+        position: "static",
+        background: "none",
+        boxShadow: "none"
+    }}
+    >
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+            <FlexBetween>
+                <IconButton onClick={() => console.log("close/open the sidebar")}>
+                    <Toggle></Toggle>
+                </IconButton>
+                <FlexBetween
+                backgroundColor={theme.palette.background.alt}
+                borderRadius="10px"
+                gap="6rem"
+                p="0.1rem 1.5rem"
+                >
+                    <InputBase placeholder='Search...'></InputBase>
+                    <IconButton>
+                        <Search></Search>
+                    </IconButton>
+                </FlexBetween>
+            </FlexBetween>
+
+
+            <FlexBetween gap="1.5rem">
+                <IconButton onClick={() => dispatch(setMode())}>
+                    {theme.palette.mode === "dark" ? (<DarkModeOutlined sx={{ fontSize: "25px" }}></DarkModeOutlined>) : (<LightModeOutlined sx={{ fontSize: "25px" }}></LightModeOutlined>)}
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlined sx={{ fontSize: "25px" }}></SettingsOutlined>
+                </IconButton>
+            </FlexBetween>
+        </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
