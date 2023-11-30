f4c22a1eb1b010d27f00203d2d97d5e30f31e757
Magdy156 <magdyhamdy156@gmail.com>
2023-11-30 23:53:33 +0200

    adding user data to DB and connecting them with frontend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import mongoose from "mongoose";
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js"
 
 // ROUTES
 
@@ -11,6 +13,7 @@ import clientRoutes from "./routes/client.js"
 import generalRoutes from "./routes/general.js"
 import salesRoutes from "./routes/sales.js"
 import manageRoutes from "./routes/manage.js"
+
 
 // CONFIGURATIONS & MiddleWares
 
@@ -23,6 +26,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(cors());
 
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", manageRoutes);
+app.use("/sales", salesRoutes);
 // MONGO Connection
 const port = process.env.PORT || 8000;
 mongoose.connect(process.env.MONGO_URL, {
@@ -30,4 +37,6 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(port, () => console.log(`server works on port ${port}`))
+    // make sure to insert the data to the DB one time
+    // User.insertMany(dataUser);
 }).catch((err) => console.log(err))
