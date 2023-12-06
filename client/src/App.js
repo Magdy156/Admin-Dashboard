57fcf4081316c4fcc688c517884f81f98ad3ae93
Magdy156 <magdyhamdy156@gmail.com>
2023-12-07 00:39:28 +0200

    finishing the customers Page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import { themeSettings } from "theme";
 import Dashboard from "scenes/dashboard";
 import Products from "scenes/products";
+import Customers from "scenes/customers"
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "scenes/layout";
 
@@ -21,6 +22,7 @@ function App(){
                         <Route path="/" element={<Navigate to="/dashboard"  replace/>}></Route>
                         <Route path="/dashboard" element={<Dashboard />}></Route>
                         <Route path="/products" element={<Products />}></Route>
+                        <Route path="/customers" element={<Customers />}></Route>
                     </Route>
                 </Routes>
             </ThemeProvider>
